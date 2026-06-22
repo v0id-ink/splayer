8a2b3d31b94588526a848b68e725b27edb403e3d
顾雨晨 <liu_qinyu@outlook.com>
2026-06-22 22:59:05 +0800

    fix: play-music share redirects to home immediately
    no more blank loading page, straight to home while playing shared song

--- src/views/PlayMusic.vue
+++ src/views/PlayMusic.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="play-music-loading">
-    <n-spin v-if="loading" size="medium">
-      <template #description>正在加载分享歌曲...</template>
-    </n-spin>
-  </div>
+  <div />
 </template>
 
 <script setup lang="ts">
@@ -19,14 +15,13 @@ const player = usePlayerController();
 const statusStore = useStatusStore();
 const dataStore = useDataStore();
 
-const loading = ref(true);
-
 onMounted(async () => {
+  // 立即回主页，后台加载播放
+  router.replace("/");
+
   const id = Number(route.query.id);
   if (!id) {
-    loading.value = false;
     window.$message.error("分享的歌曲不存在");
-    router.replace("/");
     return;
   }
 
@@ -39,26 +34,12 @@ onMounted(async () => {
     }
 
     const song = songs[0];
-    // 清空并设置播放列表
     dataStore.playList = songs;
     statusStore.playIndex = 0;
-    // 显示全屏播放器并播放
     statusStore.showFullPlayer = true;
     player.playSong({ song });
   } catch {
     window.$message.error("分享的歌曲不存在");
-    router.replace("/");
-  } finally {
-    loading.value = false;
   }
 });
 </script>
-
-<style lang="scss" scoped>
-.play-music-loading {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-</style>
